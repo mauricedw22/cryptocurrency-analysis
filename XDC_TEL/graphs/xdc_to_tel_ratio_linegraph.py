45a70423d9fddb1288eb2c0372538b9152b0d75b
Maurice WALKER <mauricedw22@gmail.com>
2021-10-05 11:18:43 -0400

    added xdc_tel ratio graph 1

---
+++ XDC_TEL/graphs/xdc_to_tel_ratio_linegraph.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import matplotlib.dates as dates
+
+priceData = pd.read_csv('https://crypto-pricing-data.s3.us-east-2.amazonaws.com/tel-xdc-04_15_18-10_03_21.csv')
+
+xdc_tel_ratios = priceData[['xdc_tel_ratio']]
+time = priceData[['xdc_date']]
+
+x_values = time
+y_values = xdc_tel_ratios
+
+new_x = dates.datestr2num(x_values)
+
+plt.xlabel('Date')
+plt.ylabel('XDC to TEL Ratio')
+plt.title('XDC to TEL over time')
+
+plt.plot_date(new_x, y_values, fmt="b-", tz=None, xdate=True)
+
+plt.legend()
+
+plt.show()
+
+""" Programmatic save of file
+
+plt.savefig('xdc_tel_ratios_graph.png', format='png')
+
+"""
