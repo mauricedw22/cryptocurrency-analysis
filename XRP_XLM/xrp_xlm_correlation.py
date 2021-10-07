426c06b01bc9ac0ff254f0aa76f1718b60bd6b6f
Maurice WALKER <mauricedw22@gmail.com>
2021-10-07 11:44:17 -0400

    added correlation script

---
+++ XRP_XLM/xrp_xlm_correlation.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+correlationData = pd.read_csv('https://crypto-pricing-data.s3.us-east-2.amazonaws.com/xrp-xlm-01_01_18-10_07_21.csv')
+
+correlation1 = correlationData['xrp_price'].corr(correlationData['xlm_price'])
+correlation2 = correlationData['xrp_total_volume'].corr(correlationData['xlm_total_volume'])
+
+correlation3 = correlationData['xrp_price'].corr(correlationData['xrp_total_volume'])
+correlation4 = correlationData['xlm_price'].corr(correlationData['xlm_total_volume'])
+
+print('XRP-XLM price corr: ' + str(correlation1))
+print('XRP-XLM volume corr: ' + str(correlation2))
+print('XRP price-to-volume corr: ' + str(correlation3))
+print('XLM price-to-volume corr: ' + str(correlation4))
