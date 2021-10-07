233a724528186c7c5824ba88209bbe1997d05c9f
Maurice WALKER <mauricedw22@gmail.com>
2021-10-07 11:46:46 -0400

    added 2nd xrp-xlm graph

---
+++ XRP_XLM/graphs/xrp_to_xlm_ratio_linegraph.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import matplotlib.dates as dates
+
+priceData = pd.read_csv('https://crypto-pricing-data.s3.us-east-2.amazonaws.com/xrp-xlm-01_01_18-10_07_21.csv')
+
+xrp_xlm_ratios = priceData[['xrp_xlm_ratio']]
+time = priceData[['xrp_date']]
+
+x_values = time
+y_values = xrp_xlm_ratios
+
+new_x = dates.datestr2num(x_values)
+
+plt.xlabel('Date')
+plt.ylabel('XRP to XLM Ratio')
+plt.title('XRP to XLM over time')
+
+plt.plot_date(new_x, y_values, fmt="b-", tz=None, xdate=True)
+
+plt.legend()
+
+plt.show()
+
+""" Programmatic save of file
+
+plt.savefig('xdc_tel_ratios_graph.png', format='png')
+
+"""
