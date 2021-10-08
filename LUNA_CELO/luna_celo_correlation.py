169b165ff9713244e716bb0dbe14b84aa163f901
Maurice WALKER <mauricedw22@gmail.com>
2021-10-08 10:54:34 -0400

    edited luna celo data

--- LUNA_CELO/luna_celo_correlation.py
+++ LUNA_CELO/luna_celo_correlation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-correlationData = pd.read_csv('https://crypto-pricing-data.s3.us-east-2.amazonaws.com/luna-celo-05_22_20-10_07_21.csv')
+correlationData = pd.read_csv('../cryptoData/luna-celo-05_22_20-10_07_21.csv')
 
 correlation1 = correlationData['luna_price'].corr(correlationData['celo_price'])
 correlation2 = correlationData['luna_total_volume'].corr(correlationData['celo_total_volume'])
