dc867826b670941b13981654c34dd5824a895d2f
Maurice WALKER <mauricedw22@gmail.com>
2021-10-12 00:07:12 -0400

    xdc tel graph 1 update

--- XDC_TEL/graphs/xdc_to_tel_ratio_linegraph.py
+++ XDC_TEL/graphs/xdc_to_tel_ratio_linegraph.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.dates as dates
 
-priceData = pd.read_csv('../cryptoData/tel-xdc-04_15_18-10_03_21.csv')
+priceData = pd.read_csv('<YOUR_CSV_DATA>')
 
 xdc_tel_ratios = priceData[['xdc_tel_ratio']]
 time = priceData[['xdc_date']]
